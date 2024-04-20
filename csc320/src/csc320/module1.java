9cd187510388f0753680ceb947bdce8da6a8e2de
hwint <hwint@Winterton>
2024-04-20 09:39:31 -0600

    First commit

---
+++ csc320/src/csc320/module1.java
@@ -0,0 +1,17 @@
+package csc320;
+
+public class module1 {
+	public static void main(String[] args) {
+		String firstName = "Paul";
+		String lastName = "Atreides";
+		String streetAddress = "10176 Freman Lane";
+		String city = "Arrakis";
+		String zipCode = "10214";
+		
+		System.out.println("First name: " + firstName);
+		System.out.println("Last name: " + lastName);
+		System.out.println("Address: " + streetAddress);
+		System.out.println("City: " + city);
+		System.out.println("Zipcode: " + zipCode);
+	}
+	}
